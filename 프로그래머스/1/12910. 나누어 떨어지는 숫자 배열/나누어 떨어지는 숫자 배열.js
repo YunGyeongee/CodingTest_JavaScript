3ccfcfebe05c3105482a412d3bef6e5a93321900
YunGyeong Kim <86589216+YunGyeongee@users.noreply.github.com>
2024-01-02 09:26:53 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
@@ -0,0 +1,14 @@
+function solution(arr, divisor) {
+    let result = []
+    arr.forEach((item) => {
+        if (item % divisor === 0) {
+            result.push(item)
+        }
+    })
+
+    if (result.length < 1) {
+        result.push(-1)
+    }
+    
+    return result.sort((a, b) => a - b)
+}
